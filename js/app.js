0bcb0c30c282d3227cd258d439075b5ed4e14eae
Shivam Nishad <shivam.n246@gmail.com>
2021-01-19 00:01:33 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots'
+
+
+    });
+});
+
+$(document).ready(function(){
+    $('.Certificate-slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.Certificate-slider-dots',
+        dotsClass:'Certificate-dots'
+
+
+    });
+});
